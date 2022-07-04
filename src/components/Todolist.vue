bc59fab09909dd85953b4be528e3e9a33a77ec7d
jaichien <esther.chien@cloudmatrix.com.tw>
2022-07-04 11:12:44 +0800

    fetch: ts logic for done or doing action.

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -4,33 +4,68 @@
       <b-row>
 
         <b-input-group prepend="待辦事項" class="mt-3">
-          <b-form-input placeholder="準備要做的事情"></b-form-input>
-          <b-input-group-append>
+          <b-form-input placeholder="準備要做的事情" v-model="text"></b-form-input>
+          <b-input-group-append @click="appendItem()">
             <b-button variant="success">新增</b-button>
           </b-input-group-append>
         </b-input-group>
+
         <b-tabs content-class="mt-3" class="mt-3">
           <b-tab title="全部" active>
             
             <b-list-group>
-              <b-list-group-item>
+              
+              <b-list-group-item class="d-flex justify-content-between align-items-center" v-for="(item, index) in todolist">
                 <b-form-checkbox
-                  v-model="status"
-                  value="accepted"
-                  unchecked-value="not_accepted">
-                  123
+                  v-model="item.isSelected">
+                  <span :class="{ 'item-done':item.isSelected, 'item-normal':!item.isSelected }">{{item.text}}</span>
                 </b-form-checkbox>
+                <div class="pointer" @click="deleteItem(index)">
+                  <font-awesome-icon icon="fa-solid fa-trash" />
+                </div>
+              </b-list-group-item>
+
+            </b-list-group>
+
+          </b-tab>
+          <b-tab title="進行">
+            <b-list-group>
+              
+              <b-list-group-item class="d-flex justify-content-between align-items-center" 
+                v-for="(item, index) in getlist(false)">
+                <b-form-checkbox
+                  v-model="item.isSelected">
+                  <span :class="{ 'item-done':item.isSelected, 'item-normal':!item.isSelected }">{{item.text}}</span>
+                </b-form-checkbox>
+                <div class="pointer" @click="deleteItem(index)">
+                  <font-awesome-icon icon="fa-solid fa-trash" />
+                </div>
+              </b-list-group-item>
+
+            </b-list-group>
+
+          </b-tab>
+          <b-tab title="已完成">
+
+            <b-list-group>
+              <b-list-group-item class="d-flex justify-content-between align-items-center" 
+                v-for="(item, index) in getlist(true)"  v-show="item.isSelected">
+                <b-form-checkbox
+                  v-model="item.isSelected">
+                  <span :class="{ 'item-done':item.isSelected, 'item-normal':!item.isSelected }">{{item.text}}</span>
+                </b-form-checkbox>
+                <div class="pointer" @click="deleteItem(index)">
+                  <font-awesome-icon icon="fa-solid fa-trash" />
+                </div>
               </b-list-group-item>
             </b-list-group>
 
           </b-tab>
-          <b-tab title="進行"><p>I'm the second tab</p></b-tab>
-          <b-tab title="已完成"><p>I'm a disabled tab!</p></b-tab>
         </b-tabs>
         <b-container class="bv-example-row">
           <b-row content-class="mt-3" class="mt-2">
-            <b-col style="text-align: left;" class="mt-2">還有 N 筆任務未完成</b-col>
-            <b-col class="right-col"><p class="right-btn clear">清除所有任務</p></b-col>
+            <b-col style="text-align: left;" class="mt-2">還有 {{getlist(false).length}} 筆任務未完成</b-col>
+            <b-col class="right-col" @click="clearAllItem()"><p class="right-btn pointer">清除所有任務</p></b-col>
           </b-row>
         </b-container>
       </b-row>
@@ -44,17 +79,39 @@ import { Options, Vue } from 'vue-class-component';
 @Options({
   props: {
     msg: String,
-    status: Boolean,
   }
 })
 
 export default class Todolist extends Vue {
   msg!: string
   status = false
+  todolist = new Array()
+  text = null
 
-  itemClick(){
-    this.status=!this.status
-    console.log("item click", this.status)
+  getlist(isDone:boolean){
+    return this.todolist.filter(function(item) {
+      return item.isSelected==isDone
+    })
+  }
+
+  clearAllItem(){
+    this.todolist = new Array()
+  }
+
+  deleteItem(index:number){
+    console.log("item click", index)
+    if (index > -1) {
+       this.todolist.splice(index, 1);
+    }
+  }
+  
+  appendItem(){
+    this.todolist.push({ "id": this.todolist.length, "text":this.text, isSelected:false })
+    this.text = null
+  }
+  
+  created(){
+    console.log("created now")
   }
 }
 </script>
@@ -68,7 +125,13 @@ export default class Todolist extends Vue {
   position: relative !important;
   float: right !important;
 }
-.right-btn.clear { 
+.item-done {
+  text-decoration: line-through;
+}
+.item-normal {
+  text-decoration: null;
+}
+.pointer {
   cursor: pointer;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,22 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+/* add icons to the library */
+library.add(faTrash)
+
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(BootstrapVue3)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
